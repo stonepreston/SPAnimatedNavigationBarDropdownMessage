edbc2554b6ebe854902e8759210c9b019951c9a1
Stone Preston <ruerue45>
2014-04-06 17:30:02 -0500

    Initial Commit

---
+++ SPAnimatedNavigationBarDropdownMessage/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  SPAnimatedNavigationBarDropdownMessage
+//
+//  Created by Stone  Preston on 4/6/14.
+//  Copyright (c) 2014 Stone  Preston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
